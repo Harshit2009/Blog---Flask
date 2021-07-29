02ec50981ee180d35d8e1ee958d5f0c5daaad80e
Harshit2009 <09harshitgoel@gmail.com>
2021-07-29 10:54:16 +0530

    Changes

---
+++ FTSEntry.py
@@ -0,0 +1,31 @@
+import datetime
+import functools
+import os
+import re
+import urllib
+from Entry import *
+
+from flask import (Flask, flash, Markup, redirect, render_template, request,
+                   Response, session, url_for)
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+from peewee import *
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.sqlite_ext import *
+
+class FTSEntry(FTSModel):
+    content = TextField()
+
+    class Meta:
+        database = database
+
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import functools
 import os
 import re
 import urllib
+from Entry import *
+from FTSEntry import *
 
 from flask import (Flask, flash, Markup, redirect, render_template, request,
                    Response, session, url_for)
@@ -14,111 +16,6 @@ from micawber.cache import Cache as OEmbedCache
 from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
-
-ADMIN_PASSWORD = 'secret'
-APP_DIR = os.path.dirname(os.path.realpath(__file__))
-
-DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
-DEBUG = False
-
-SECRET_KEY = 'shhh, secret!'
-
-SITE_WIDTH = 800
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-flask_db = FlaskDB(app)
-
-database = flask_db.database
-
-oembed_providers = bootstrap_basic(OEmbedCache())
-
-class Entry(flask_db.Model):
-    title = CharField()
-    slug = CharField(unique=True)
-    content = TextField()
-    published = BooleanField(index=True)
-    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
-
-    @property
-    def html_content(self):
-        """
-        Generate HTML representation of the markdown-formatted blog entry,
-        and also convert any media URLs into rich media objects such as video
-        players or images.
-        """
-        hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
-        extras = ExtraExtension()
-        markdown_content = markdown(self.content, extensions=[hilite, extras])
-        oembed_content = parse_html(
-            markdown_content,
-            oembed_providers,
-            urlize_all=True,
-            maxwidth=app.config['SITE_WIDTH'])
-        return Markup(oembed_content)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = re.sub(r'[^\w]+', '-', self.title.lower()).strip('-')
-        ret = super(Entry, self).save(*args, **kwargs)
-
-        self.update_search_index()
-        return ret
-
-    def update_search_index(self):
-        exists = (FTSEntry
-                  .select(FTSEntry.docid)
-                  .where(FTSEntry.docid == self.id)
-                  .exists())
-        content = '\n'.join((self.title, self.content))
-        if exists:
-            (FTSEntry
-             .update({FTSEntry.content: content})
-             .where(FTSEntry.docid == self.id)
-             .execute())
-        else:
-            FTSEntry.insert({
-                FTSEntry.docid: self.id,
-                FTSEntry.content: content}).execute()
-
-    @classmethod
-    def public(cls):
-        return Entry.select().where(Entry.published == True)
-
-    @classmethod
-    def drafts(cls):
-        return Entry.select().where(Entry.published == False)
-
-    @classmethod
-    def search(cls, query):
-        words = [word.strip() for word in query.split() if word.strip()]
-        if not words:
-            return Entry.noop()
-        else:
-            search = ' '.join(words)
-
-        return (Entry
-                .select(Entry, FTSEntry.rank().alias('score'))
-                .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
-                .where(
-                    FTSEntry.match(search) &
-                    (Entry.published == True))
-                .order_by(SQL('score')))
-
-class FTSEntry(FTSModel):
-    content = TextField()
-
-    class Meta:
-        database = database
-
-def login_required(fn):
-    @functools.wraps(fn)
-    def inner(*args, **kwargs):
-        if session.get('logged_in'):
-            return fn(*args, **kwargs)
-        return redirect(url_for('login', next=request.path))
-    return inner
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -134,6 +31,7 @@ def login():
         else:
             flash('Incorrect password.', 'danger')
     return render_template('login.html', next_url=next_url)
+
 
 @app.route('/logout/', methods=['GET', 'POST'])
 def logout():
